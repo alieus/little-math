f7ce311e1d4ef93edaa9d2da1ff7d8dade0e155d
ktr-kl <catrina_rm@yahoo.gr>
2015-12-15 15:47:27 +0200

    Create android project

---
+++ app/src/main/java/stathis_katerina/little_math/LittleMath.java
@@ -0,0 +1,13 @@
+package stathis_katerina.little_math;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LittleMath extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_little_math);
+    }
+}
