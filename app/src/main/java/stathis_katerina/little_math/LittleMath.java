11bfce4ca0914b078c0bb5ae29928ae3d1dc9a05
ktr-kl <catrina_rm@yahoo.gr>
2016-01-08 13:02:05 +0200

    Main menu and navigation

--- app/src/main/java/stathis_katerina/little_math/LittleMath.java
+++ app/src/main/java/stathis_katerina/little_math/LittleMath.java
@@ -1,10 +1,19 @@
 package stathis_katerina.little_math;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class LittleMath extends AppCompatActivity {
+import layout.Power;
+
+public class LittleMath extends FragmentActivity {
+
+    private static final String MATH = "math";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +21,42 @@ public class LittleMath extends AppCompatActivity {
         setContentView(R.layout.activity_little_math);
     }
 
-    public void launchPower(View view) {
+    void putFragmentToContent(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment, MATH);
+        fragmentTransaction.commit();
+        findViewById(R.id.menu).setVisibility(View.GONE);
+    }
 
+    void showMenu() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(MATH);
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+        }
+        findViewById(R.id.menu).setVisibility(View.VISIBLE);
+    }
+
+    public void launchPower(View view) {
+        putFragmentToContent(new Power());
     }
 
     public void launchEquation(View view) {
 
+    }
+
+    boolean isOnMenu() {
+        return findViewById(R.id.menu).getVisibility() == View.VISIBLE;
+    }
+
+    public void onBackPressed() {
+        if (isOnMenu()) {
+            super.onBackPressed();
+        } else {
+            showMenu();
+        }
     }
 
 }
